f51a859532c50029eb2fc5c871ea3b75c2f7b1af
Osman Jalloh <osman.jalloh@gmail.com>
2023-08-24 18:57:07 -0700

    Changes to the Web app API to reflect changes to iOS and Android

--- jannah_graphql/jannah_site/queries.py
+++ jannah_graphql/jannah_site/queries.py
@@ -1,100 +1,100 @@
 import graphene
-from collections import namedtuple
 from jannah_site import models as jannah_models
 from jannah_site import types as jannah_types
 
-WorkflowsValueObject = namedtuple("Workflows", [
-                                                "cursor",
-                                                "hasMore",
-                                                "workflows"
-                                                ]
-                                  )
-
-class JannahWorkflows(graphene.ObjectType):
+class JannahAPIObjectType(graphene.ObjectType):
     cursor = graphene.Int()
     hasMore = graphene.Boolean()
-    workflows = graphene.List(jannah_types.WorkflowType)
-    # sites = graphene.List(jannah_types.JannahSiteType)
-    # users = graphene.List(jannah_types.UserType)
-    # boots = graphene.List(jannah_types.BootType)
-    # networks = graphene.List(jannah_types.NetworkType)
-    # storages = graphene.List(jannah_types.StorageType)
-    # computes = graphene.List(jannah_types.ComputeType)
-    # uxs = graphene.List(jannah_types.UXType)
-    # feedbacks = graphene.List(jannah_types.FeedbackType)
 
+class JannahSites(JannahAPIObjectType):
+    sites = graphene.List(jannah_types.SiteType)
+
+class JannahUsers(JannahAPIObjectType):
+    users = graphene.List(jannah_types.UserType)
+
+class JannahBoots(JannahAPIObjectType):
+    boots = graphene.List(jannah_types.BootType)
+
+class JannahNetworks(JannahAPIObjectType):
+    networks = graphene.List(jannah_types.NetworkType)
+
+class JannahStorages(JannahAPIObjectType):
+    storages = graphene.List(jannah_types.StorageType)
+
+class JannahComputes(JannahAPIObjectType):
+    computes = graphene.List(jannah_types.ComputeType)
+
+class JannahUXs(JannahAPIObjectType):
+    uxs = graphene.List(jannah_types.UXType)
+
+class JannahFeedbacks(JannahAPIObjectType):
+    feedbacks = graphene.List(jannah_types.FeedbackType)
+
+class JannahWorkflows(JannahAPIObjectType):
+    workflows = graphene.List(jannah_types.WorkflowType)
 
 class Query(graphene.ObjectType):
-    site = graphene.Field(jannah_types.JannahSiteType)
-    sites = graphene.List(jannah_types.JannahSiteType)
-    users = graphene.List(jannah_types.UserType)
-    boots = graphene.List(jannah_types.BootType)
-    networks = graphene.List(jannah_types.NetworkType)
-    storages = graphene.List(jannah_types.StorageType)
-    computes = graphene.List(jannah_types.ComputeType)
-    uxs = graphene.List(jannah_types.UXType)
-    feedbacks = graphene.List(jannah_types.FeedbackType)
-#    workflows = graphene.ObjectType(jannah_types.WorkflowType)
+    sites = graphene.Field(JannahSites)
+    users = graphene.Field(JannahUsers)
+    boots = graphene.Field(JannahBoots)
+    networks = graphene.Field(JannahNetworks)
+    storages = graphene.Field(JannahStorages)
+    computes = graphene.Field(JannahComputes)
+    uxs = graphene.Field(JannahUXs)
+    feedbacks = graphene.Field(JannahFeedbacks)
     workflows = graphene.Field(JannahWorkflows)
 
-    def resolve_workflows(parent, info):
-        return JannahWorkflows(
-                                cursor = 8,
-                                hasMore = True,
-                                workflows = jannah_models.Workflow.objects.all()
-                                # sites = jannah_models.Site.objects.all(),
-                                # users = jannah_models.User.objects.all(),
-                                # boots = jannah_models.Boot.objects.all(),
-                                # networks = jannah_models.Network.objects.all(),
-                                # storages = jannah_models.Storage.objects.all(),
-                                # computes = jannah_models.Compute.objects.all(),
-                                # uxs = jannah_models.UX.objects.all(),
-                                # feedbacks = jannah_models.Feedback.objects.all()
-                            )
-
-    def resolve_site(root, info):
-        return (
-            jannah_models.Site.objects.first()
-        )
     def resolve_sites(root, info):
-            return (
-                jannah_models.Site.objects.all()
+        return JannahSites(
+                cursor = 8,
+                hasMore = True,
+                sites = jannah_models.Site.objects.all()
             )
     def resolve_users(root, info):
-        return (
-            jannah_models.User.objects.all()
+        return JannahUsers(
+            cursor = 8,
+            hasMore = True,
+            users = jannah_models.User.objects.all()
         )
     def resolve_boots(root, info):
-        return (
-                jannah_models.Boot.objects.all()
+        return JannahBoots(
+                cursor = 8,
+                hasMore = True,
+                boots = jannah_models.Boot.objects.all()
             )
     def resolve_networks(root, info):
-        return (
-            jannah_models.Network.objects.all()
+        return JannahNetworks(
+            cursor = 8,
+            hasMore = True,
+            networks = jannah_models.Network.objects.all()
         )
-
     def resolve_storages(root, info):
-        return (
-            jannah_models.Storage.objects.all()
+        return JannahStorages(
+            cursor = 8,
+            hasMore = True,
+            storages = jannah_models.Storage.objects.all()
         )
-
     def resolve_computes(root, info):
-        return (
-            jannah_models.Compute.objects.all()
+        return JannahComputes(
+            cursor = 8,
+            hasMore = True,
+            computes = jannah_models.Compute.objects.all()
         )
-
     def resolve_uxs(root, info):
-        return (
-            jannah_models.UX.objects.all()
+        return JannahUXs(
+            cursor = 8,
+            hasMore = True,
+            uxs = jannah_models.UX.objects.all()
         )
-
     def resolve_feedbacks(root, info):
-        return (
-            jannah_models.Feedback.objects.all()
+        return JannahFeedbacks(
+            cursor = 8,
+            hasMore = True,
+            feedbacks = jannah_models.Feedback.objects.all()
         )
-
-    # def resolve_workflows(root, info):
-    #     return {
-    #         'cursor': 8,
-    #         'workflows': jannah_models.Workflow.objects.all()
-    #     }
+    def resolve_workflows(parent, info):
+        return JannahWorkflows(
+            cursor = 8,
+            hasMore = True,
+            workflows = jannah_models.Workflow.objects.all()
+        )
