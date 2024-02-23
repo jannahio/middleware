07f04f7f4b3411061f21c7c769af4a77feddfdb7
Osman Jalloh <osman.jalloh@gmail.com>
2024-02-22 19:35:43 -0800

    POC for component pipeline

--- jannah_graphql/jannah_site/pipelines/my_component.py
+++ jannah_graphql/jannah_site/pipelines/my_component.py
@@ -6,3 +6,18 @@ from math_utils import add_numbers
                target_image='jannahioregistry/jannah-component-2-arm64-ubuntu:v0.0.1')
 def add(a: int, b: int) -> int:
     return add_numbers(a, b)
+
+@dsl.container_component
+def say_hello(name: str, greeting: dsl.OutputPath(str)):
+    """Log a greeting and return it as an output."""
+
+    return dsl.ContainerSpec(
+        image='alpine',
+        command=[
+            'sh', '-c', '''RESPONSE="Hello, $0!"\
+                            && echo $RESPONSE\
+                            && mkdir -p $(dirname $1)\
+                            && echo $RESPONSE > $1
+                            '''
+        ],
+        args=[name, greeting])
--- jannah_graphql/jannah_site/pipelines/pipeline.py
+++ jannah_graphql/jannah_site/pipelines/pipeline.py
@@ -1,6 +1,6 @@
 import kfp
 from kfp import dsl
-from my_component import add
+from my_component import (add, say_hello)
 import sys
 
 #addition_pipeline_path ="/Users/osmanjalloh/working/operator/tmp/EPHEMERAL/images/ubuntu/middleware/jannah_graphql/jannah_site/addition-pipeline-2024-02-16T19-53-01-717858.yaml"
@@ -11,9 +11,23 @@ def addition_pipeline(x: int, y: int) -> int:
     task2 = add(a=task1.output, b=x)
     return task2.output
 
-def main(addition_pipeline_path):
+
+@dsl.pipeline
+def hello_pipeline(person_to_greet: str) -> str:
+    # greeting argument is provided automatically at runtime!
+    hello_task = say_hello(name=person_to_greet)
+    return hello_task.outputs['greeting']
+
+def main(
+            addition_pipeline_path,
+            hello_pipeline_path,
+        ):
     kfp.compiler.Compiler().compile(addition_pipeline, addition_pipeline_path)
+    kfp.compiler.Compiler().compile(hello_pipeline, hello_pipeline_path)
 
 if __name__ == '__main__':
     print(sys.argv)
-    main(sys.argv[1])
+    main(
+            sys.argv[1],
+            sys.argv[2]
+        )
