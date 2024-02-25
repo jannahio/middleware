0986e00279daf30e16a3810403189184ddca8f35
Osman Jalloh <osman.jalloh@gmail.com>
2024-02-25 14:59:18 -0800

    A two phase approach to build the Kubeflow pipeline container image. First we build the container image with Docker off alpine

--- jannah_graphql/jannah_site/pipelines/my_component.py
+++ jannah_graphql/jannah_site/pipelines/my_component.py
@@ -1,9 +1,21 @@
+import os
 from kfp import dsl
 from math_utils import add_numbers
 
+_base_image='python:3.7'
+_target_image='jannahioregistry/jannah-component-2-arm64-ubuntu:v0.0.1'
 
-@dsl.component(base_image='python:3.7',
-               target_image='jannahioregistry/jannah-component-2-arm64-ubuntu:v0.0.1')
+if os.environ.get('JANNAH_PIPELINE_BASE_IMAGE'):
+  _base_image = os.environ['JANNAH_PIPELINE_BASE_IMAGE']
+if os.environ.get('JANNAH_PIPELINE_TARGET_IMAGE'):
+  _target_image = os.environ['JANNAH_PIPELINE_TARGET_IMAGE']
+
+
+print(_base_image)
+print(_target_image)
+
+@dsl.component(base_image=_base_image,
+               target_image=_target_image)
 def add(a: int, b: int) -> int:
     return add_numbers(a, b)
 
